c8c3553b78944e1fe4765c67a89bfa42ea8e077d
Max Lapp <max.lapp@skbkontur.ru>
2014-11-19 15:42:50 +0500

    ConnectorAPI: Добавил новый тип транспортных ящиков для взаимодействия с коннекторами.

---
+++ Http/Connectors/ConnectorEdiApiClient.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+
+using KonturEdi.Api.Client.Http.Helpers;
+using KonturEdi.Api.Types;
+using KonturEdi.Api.Types.BoxEvents;
+using KonturEdi.Api.Types.Common;
+using KonturEdi.Api.Types.Connectors;
+using KonturEdi.Api.Types.Serialization;
+
+namespace KonturEdi.Api.Client.Http.Connectors
+{
+    public abstract class ConnectorEdiApiClient<TBoxEventBatch, TBoxEventType, TBoxEvent> : BaseEdiApiHttpClient<TBoxEventBatch, TBoxEventType, TBoxEvent>, IConnectorEdiApiClient<TBoxEventBatch, TBoxEventType, TBoxEvent>
+        where TBoxEventType : struct
+        where TBoxEvent : BoxEvent<TBoxEventType>
+        where TBoxEventBatch : BoxEventBatch<TBoxEventType, TBoxEvent>
+    {
+        protected ConnectorEdiApiClient(IBoxEventTypeRegistry<TBoxEventType> boxEventTypeRegistry, string apiClientId, Uri baseUri, IEdiApiTypesSerializer serializer, int timeoutInMilliseconds, IWebProxy proxy = null)
+            : base(boxEventTypeRegistry, apiClientId, baseUri, serializer, timeoutInMilliseconds, proxy)
+        {
+        }
+
+        public MessageEntity GetMessage(string authToken, string boxId, string messageId)
+        {
+            var url = new UrlBuilder(BaseUri, RelativeUrl + "GetMessage")
+                .AddParameter("boxId", boxId)
+                .AddParameter("messageId", messageId)
+                .ToUri();
+            return MakeGetRequest<MessageEntity>(url, authToken);
+        }
+
+        public ConnectorBoxesInfo GetConnectorBoxesInfo(string authToken)
+        {
+            var url = new UrlBuilder(BaseUri, "V1/Boxes/GetConnectorBoxesInfo").ToUri();
+            return MakeGetRequest<ConnectorBoxesInfo>(url, authToken);
+        }
+    }
+}
---
+++ IConnectorEdiApiClient.cs
@@ -0,0 +1,15 @@
+﻿using KonturEdi.Api.Types.BoxEvents;
+using KonturEdi.Api.Types.Common;
+using KonturEdi.Api.Types.Connectors;
+
+namespace KonturEdi.Api.Client
+{
+    public interface IConnectorEdiApiClient<out TBoxEventBatch, TBoxEventType, TBoxEvent> : IBaseEdiApiClient<TBoxEventBatch, TBoxEventType, TBoxEvent>
+        where TBoxEventType : struct
+        where TBoxEvent : BoxEvent<TBoxEventType>
+        where TBoxEventBatch : BoxEventBatch<TBoxEventType, TBoxEvent>
+    {
+        MessageEntity GetMessage(string authToken, string boxId, string messageId);
+        ConnectorBoxesInfo GetConnectorBoxesInfo(string authToken);
+    }
+}
