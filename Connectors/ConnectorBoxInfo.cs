441a60a75b53b0543a403de58c795e49c0fc4781
Max Lapp <max.lapp@skbkontur.ru>
2014-11-19 15:42:50 +0500

    ConnectorAPI: Добавил новый тип транспортных ящиков для взаимодействия с коннекторами.

---
+++ Connectors/ConnectorBoxInfo.cs
@@ -0,0 +1,23 @@
+﻿namespace KonturEdi.Api.Types.Connectors
+{
+    public class ConnectorBoxInfo
+    {
+        public string Id { get; set; }
+        public string PartyId { get; set; }
+        public ConnectorBoxSettings BoxSettings { get; set; }
+    }
+
+    public class ConnectorBoxSettings
+    {
+        public string Name { get; set; }
+        public ConnectorType ConnectorType { get; set; }
+    }
+
+    public enum ConnectorType
+    {
+        Unknown,
+        Router,
+        Transformer,
+        Transporter,
+    }
+}
