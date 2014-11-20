32c8b22d6567cf0a497c43d2b8e61227fe10623f
Max Lapp <max.lapp@skbkontur.ru>
2014-11-20 15:49:15 +0500

    ConnectorAPI: Добавил методы получения информации о партях.

---
+++ Parties/PartiesInfo.cs
@@ -0,0 +1,7 @@
+﻿namespace KonturEdi.Api.Types.Parties
+{
+    public class PartiesInfo
+    {
+        public PartyInfo[] Parties { get; set; }
+    }
+}
---
+++ Parties/PartyInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace KonturEdi.Api.Types.Parties
+{
+    public class PartyInfo
+    {
+        public string Id { get; set; }
+        public string Gln { get; set; }
+        public string Name { get; set; }
+    }
+}
