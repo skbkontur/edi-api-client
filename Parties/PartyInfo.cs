c97102c3c63181e8b077189fd1068ce7fb761984
Игорь <i.mamay@skbkontur.ru>
2015-01-13 19:44:42 +0500

    Добавил поля в PartyInfo. Поправил  тесты. разделил тесты на разные классы

--- Parties/PartyInfo.cs
+++ Parties/PartyInfo.cs
@@ -1,9 +1,16 @@
-﻿namespace KonturEdi.Api.Types.Parties
+﻿using System;
+
+namespace KonturEdi.Api.Types.Parties
 {
     public class PartyInfo
     {
         public string Id { get; set; }
         public string Gln { get; set; }
         public string Name { get; set; }
+        public string Inn { get; set; }
+        public string Kpp { get; set; }
+        public PartyType PartyTypeCode { get; set; }
+        public string DiadocOrgId { get; set; }
+        public DateTime? OrganizationCatalogueUpdateTime { get; set; }
     }
 }
