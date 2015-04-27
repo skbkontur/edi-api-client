c5185b1e98e311f0f42c0fe24a5879e6d0a96e66
Игорь <i.mamay@skbkontur.ru>
2015-04-27 14:19:48 +0500

    Добавил тип MessageReceivedDiadocRoamingErrorEventContent

--- Messages/BoxEvents/MessageBoxEventType.cs
+++ Messages/BoxEvents/MessageBoxEventType.cs
@@ -14,5 +14,6 @@
         DraftOfDocumentPackageSignedByMe,
         DraftOfDocumentPackageSignedBySender,
         DraftOfDocumentPackageDeletedFromDiadoc,
+        ReceivedDiadocRoamingErrorEventContent
     }
 }
---
+++ Messages/BoxEventsContents/Outbox/MessageReceivedDiadocRoamingErrorEventContent.cs
@@ -0,0 +1,7 @@
+﻿namespace KonturEdi.Api.Types.Messages.BoxEventsContents.Outbox
+{
+    public class MessageReceivedDiadocRoamingErrorEventContent : MessageDiadocEventContent
+    {
+        public string Reason { get; set; }
+    }
+}
