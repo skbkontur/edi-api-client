2024bdd167ef957c87e7ec4fbd3ab4a51061b009
borovskyav <borovskyav@skbkontur.ru>
2015-11-11 18:46:53 +0500

    Revert "Merge branch 'master' into RosAlko.CurrentRelease"
    This reverts commit 0e301ac227e5d53d51c09539acb6189ddeda256a, reversing
    changes made to bd81951c716838ce0d7662fcbac4461bfcbb890e.

--- Messages/InboxMessageMeta.cs
+++ Messages/InboxMessageMeta.cs
@@ -6,7 +6,6 @@ namespace KonturEdi.Api.Types.Messages
     {
         public string BoxId { get; set; }
         public string MessageId { get; set; }
-        public string DocumentCirculationId { get; set; }
         public DateTime SendDateTime { get; set; }
         public Partner Sender { get; set; }
         public MessageFormat MessageFormat { get; set; }
--- Messages/OutboxMessageMeta.cs
+++ Messages/OutboxMessageMeta.cs
@@ -4,6 +4,5 @@
     {
         public string BoxId { get; set; }
         public string MessageId { get; set; }
-        public string DocumentCirculationId { get; set; }
     }
 }
