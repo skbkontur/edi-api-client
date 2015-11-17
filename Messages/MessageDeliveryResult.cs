3409b621aa75255bd1f7750433f67a7c9233f3de
kamashev <kamashev@skbkontur.ru>
2015-11-17 16:44:19 +0400

    DeliveryApi: client methods

---
+++ Messages/MessageDeliveryResult.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace KonturEdi.Api.Types.Messages
+{
+    public class MessageDeliveryResult
+    {
+        public DateTime FirstDeliveryToClientTime { get; set; } // for SLA
+        public DateTime? SuccessfulDeliveryTime { get; set; }
+        public string[] DeliveryErrors { get; set; }
+    }
+}
