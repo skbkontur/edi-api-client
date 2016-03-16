5c3570c7e87fc2b796800f93ab5f57ead1709701
yuzaks <yuzaks@skbkontur.ru>
2016-03-16 14:38:37 +0500

    EDI.API: корр. счет в API для структуры организации

--- Organization/BankAccount.cs
+++ Organization/BankAccount.cs
@@ -3,6 +3,7 @@ namespace KonturEdi.Api.Types.Organization
     public class BankAccount
     {
         public string BankAccountNumber { get; set; }
+        public string CorrespondentAccountNumber { get; set; }
         public string BankId { get; set; }
         public string BankName { get; set; }
     }
