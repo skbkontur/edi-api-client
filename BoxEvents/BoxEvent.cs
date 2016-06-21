481f7c61dcf7d4e75978fe32ae078788b4d62717
andrew <Andrew.Kostousov@gmail.com>
2016-06-21 15:30:00 +0500

    + expose BoxEvent.PartyId field in api

--- BoxEvents/BoxEvent.cs
+++ BoxEvents/BoxEvent.cs
@@ -6,20 +6,10 @@ namespace KonturEdi.Api.Types.BoxEvents
         where TBoxEventType : struct
     {
         public string BoxId { get; set; }
+        public string PartyId { get; set; }
         public string EventId { get; set; }
         public DateTime EventDateTime { get; set; }
         public TBoxEventType EventType { get; set; }
         public object EventContent { get; set; }
-    }
-
-    public abstract class BoxEvent<TBoxEventType, TEventContent>
-        where TBoxEventType : struct
-        where TEventContent : IBoxEventContent
-    {
-        public string BoxId { get; set; }
-        public string EventId { get; set; }
-        public DateTime EventDateTime { get; set; }
-        public TBoxEventType EventType { get; set; }
-        public TEventContent EventContent { get; set; }
     }
 }
--- Messages/BoxEvents/MessageBoxEvent.cs
+++ Messages/BoxEvents/MessageBoxEvent.cs
@@ -5,9 +5,4 @@ namespace KonturEdi.Api.Types.Messages.BoxEvents
     public class MessageBoxEvent : BoxEvent<MessageBoxEventType>
     {
     }
-
-    public class MessageBoxEvent<TEventContent> : BoxEvent<MessageBoxEventType, TEventContent>
-        where TEventContent : IBoxEventContent
-    {
-    }
 }
