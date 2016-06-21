9ddf8e89417b09fa842290717fb49fdbc103f285
kamashev <kamashev@skbkontur.ru>
2016-06-21 17:46:07 +0400

    OrvisRoamingInfo: extend internal party info

--- Parties/InternalPartyInfo.cs
+++ Parties/InternalPartyInfo.cs
@@ -1,6 +1,34 @@
-﻿namespace KonturEdi.Api.Types.Parties
+﻿using System.Collections.Generic;
+
+using KonturEdi.Api.Types.Messages;
+
+namespace KonturEdi.Api.Types.Parties
 {
     public class InternalPartyInfo : PartyInfo
     {
+        public string ProviderId { get; set; }
+        public PartnerSettings[] TradingPartnerSettings { get; set; }
+    }
+
+    public class PartnerSettings
+    {
+        public string PartnerPartyId { get;set; }
+        public ConcretePartyRole PartnerPartyRole { get; set; }
+        public string OwnProviderId { get; set; }
+        public string PartnerProviderId { get; set; }
+        public TrafficType TrafficType { get; set; }
+        public DocumentSettings[] UsedDocumentTypes { get; set; }
+    }
+
+    public enum ConcretePartyRole
+    {
+        Supplier,
+        Buyer
+    }
+
+    public enum TrafficType
+    {
+        Test,
+        Production
     }
 }
