ebe280a4b0bb8b9ce0ee846476d0b24dc21e06b2
andrew <Andrew.Kostousov@gmail.com>
2016-06-27 14:41:53 +0500

    remove unnecessary confusing fields from LinkedDocumentInfo model

--- Internal/LinkedDocumentInfo.cs
+++ Internal/LinkedDocumentInfo.cs
@@ -1,15 +1,10 @@
-﻿using System;
-
-using KonturEdi.Api.Types.Messages;
-using KonturEdi.Api.Types.Messages.BoxEventsContents;
+﻿using KonturEdi.Api.Types.Messages;
 
 namespace KonturEdi.Api.Types.Internal
 {
     public class LinkedDocumentInfo
     {
-        public DocumentId DocumentId { get; set; } // может быть не заполнено для связей, созданных до релиза avk/linkedDocumentsInApi
         public string DocumentCirculationId { get; set; }
         public DocumentType DocumentType { get; set; }
-        public DateTime CreationDateTime { get; set; }
     }
 }
