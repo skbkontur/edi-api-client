af7766b3f10154c5062a01b2883392d9f33622d4
Chaplygin Anton <anton92nd@skbkontur.ru>
2017-01-17 17:41:41 +0500

    EdiApi method

---
+++ Connectors/ServiceMessageData.cs
@@ -0,0 +1,10 @@
+﻿namespace KonturEdi.Api.Types.Connectors
+{
+    public class ServiceMessageData
+    {
+        public string MessageId { get; set; }
+        public string MessageDetails { get; set; }
+        public string RecipientGln { get; set; }
+        public byte[] MessageBody { get; set; }
+    }
+}
