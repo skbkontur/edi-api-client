71a4bd11f448ca06a9cfbd9af2fd1577597fe588
olga.karpova <olga.karpova@skbkontur.ru>
2017-03-14 11:34:24 +0500

    PortalGroupId в InternalPartyInfo для орвиса

--- Parties/InternalPartyInfo.cs
+++ Parties/InternalPartyInfo.cs
@@ -3,6 +3,7 @@
     public class InternalPartyInfo : PartyInfo
     {
         public string ProviderPartyId { get; set; }
+        public string PortalGroupId { get; set; }
         public TradingPartnerSettings[] TradingPartnerSettings { get; set; }
     }
 }
