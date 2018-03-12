859104fd8ffcea5282d024b620df5c1c6a4eb178
avk <Andrew.Kostousov@gmail.com>
2018-03-12 23:20:06 +0500

    no more direct sending to ftp transport

--- Messages/DirectSending/MessageDirectlyToTransportSendResult.cs
+++ Messages/DirectSending/MessageDirectlyToTransportSendResult.cs
@@ -4,7 +4,6 @@
     {
         public bool Success { get; set; }
         public DirectSendTransportType TransportType { get; set; }
-        public FtpSendDetails FtpDetails { get; set; }
         public AS2SendDetails AS2Details { get; set; }
         public string Error { get; set; }
     }
