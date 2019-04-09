64b77f8111b96754fe1592032648bcb07d1e7b69
nlebedev <nlebedev@skbkontur.ru>
2019-04-09 19:22:12 +0500

    add transport type to event content

--- Messages/BoxEventsContents/Outbox/NewOutboxMessageEventContent.cs
+++ Messages/BoxEventsContents/Outbox/NewOutboxMessageEventContent.cs
@@ -1,6 +1,9 @@
-﻿namespace KonturEdi.Api.Types.Messages.BoxEventsContents.Outbox
+﻿using KonturEdi.Api.Types.Boxes;
+
+namespace KonturEdi.Api.Types.Messages.BoxEventsContents.Outbox
 {
     public class NewOutboxMessageEventContent : OutboxEventContentBase
     {
+        public TransportType TransportType { get; set; }
     }
 }
