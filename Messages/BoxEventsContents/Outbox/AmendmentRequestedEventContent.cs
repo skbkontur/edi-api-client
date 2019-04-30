6d76dea03d88cfaae4e77b4fcaba891815a420b7
aryzhkin <aryzhkin@skbkontur.ru>
2019-04-30 15:53:31 +0500

    add notnull attributes

--- Messages/BoxEventsContents/Outbox/AmendmentRequestedEventContent.cs
+++ Messages/BoxEventsContents/Outbox/AmendmentRequestedEventContent.cs
@@ -1,7 +1,10 @@
+using JetBrains.Annotations;
+
 namespace KonturEdi.Api.Types.Messages.BoxEventsContents.Outbox
 {
     public class AmendmentRequestedEventContent : OutboxDiadocEventContentBase
     {
+        [NotNull]
         public string AmendmentRequestMessage { get; set; }
     }
 }
