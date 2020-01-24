338f276f6b944dacd7f57781720b6fc9a36f2481
Anton Chaplygin <anton92nd@gmail.com>
2020-01-24 18:14:25 +0500

    create build and pack scripts

---
+++ .rebuild-nuget-package.cmd
@@ -0,0 +1,17 @@
+@echo off
+
+set SolutionName=KonturEdiApi
+
+rem reset current directory to the location of this script
+pushd "%~dp0"
+
+if exist "./bin" (
+    rd "./bin" /Q /S || exit /b 1
+)
+
+
+dotnet build --force --no-incremental --configuration Release "./%SolutionName%.sln" || exit /b 1
+
+dotnet pack --no-build --configuration Release "./%SolutionName%.sln" || exit /b 1
+
+pause
---
+++ .rebuild.cmd
@@ -0,0 +1,2 @@
+dotnet build --force --no-incremental --configuration Release ./KonturEdiApi.sln
+pause
