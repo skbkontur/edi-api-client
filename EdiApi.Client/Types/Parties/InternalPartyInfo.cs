fc5d067fc9fac3d3c6b91657215f7dc969f98af1
Рощупкин Виталий Сергеевич <vitalya.cookie@skbkontur.ru>
2020-04-20 20:39:06 +0500

    Add InternalPartyInfo.BillingAccountId, which is equivalent to InternalPartyInfo.PortalGroupId. PortalGroupId is obsolete now

--- EdiApi.Client/Types/Parties/InternalPartyInfo.cs
+++ EdiApi.Client/Types/Parties/InternalPartyInfo.cs
@@ -1,9 +1,15 @@
-﻿namespace SkbKontur.EdiApi.Client.Types.Parties
+using System;
+
+namespace SkbKontur.EdiApi.Client.Types.Parties
 {
     public class InternalPartyInfo : PartyInfo
     {
         public string ProviderPartyId { get; set; }
+
+        [Obsolete("Use BillingAccountId instead. These fields are equivalent and PortalGroupId will be removed in future releases.")]
         public string PortalGroupId { get; set; }
+
+        public string BillingAccountId { get; set; }
         public TradingPartnerSettings[] TradingPartnerSettings { get; set; }
     }
 }
