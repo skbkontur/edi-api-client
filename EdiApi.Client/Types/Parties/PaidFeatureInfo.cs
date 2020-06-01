04f2180e217e367da6c1769ed5776163f8729391
Vitaliy Roshcupkin <vitalya.cookie@gmail.com>
2020-06-01 15:55:29 +0500

    add draft of PaidFeatureInfo contract

---
+++ EdiApi.Client/Types/Parties/PaidFeatureInfo.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace SkbKontur.EdiApi.Client.Types.Parties
+{
+    public class PaidFeatureInfo
+    {
+        public string BillingAccountId { get; set; }
+        public PaidFeatureType PaidFeatureType { get; set; }
+        public PaidPeriod[] PaidPeriods { get; set; }
+        public DateTime? GracePeriodEnd { get; set; }
+    }
+
+    public enum PaidFeatureType
+    {
+        Unknown = 0,
+        Module1C = 1,
+        FtpTransport = 2
+    }
+
+    public class PaidPeriod
+    {
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+    }
+}
