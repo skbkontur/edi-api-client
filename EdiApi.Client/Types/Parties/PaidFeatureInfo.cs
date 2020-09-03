d5034ceec863cf1ce3b858a923ed57b367fcde87
Vitaliy Roshcupkin <vitalya.cookie@gmail.com>
2020-09-03 15:59:50 +0500

    move classes to separate files

--- EdiApi.Client/Types/Parties/PaidFeatureInfo.cs
+++ EdiApi.Client/Types/Parties/PaidFeatureInfo.cs
@@ -9,17 +9,4 @@ namespace SkbKontur.EdiApi.Client.Types.Parties
         public PaidPeriod[] PaidPeriods { get; set; }
         public DateTime? GracePeriodEnd { get; set; }
     }
-
-    public enum PaidFeatureType
-    {
-        Unknown = 0,
-        Module1C = 1,
-        FtpTransport = 2
-    }
-
-    public class PaidPeriod
-    {
-        public DateTime? StartDate { get; set; }
-        public DateTime? EndDate { get; set; }
-    }
 }
