93873b3223fee3efffbab55f147bcce919deb413
RGleb <rebrocast2@mail.ru>
2022-02-09 14:26:06 +0500

    rename: DiadocPowerOfAttorneyValidationStatusErrors -> DiadocPowerOfAttorneyValidationStatusError

--- EdiApi.Client/Types/Messages/BoxEventsContents/DiadocPowerOfAttorneyValidationStatusError.cs
+++ EdiApi.Client/Types/Messages/BoxEventsContents/DiadocPowerOfAttorneyValidationStatusError.cs
@@ -1,6 +1,6 @@
 ﻿namespace SkbKontur.EdiApi.Client.Types.Messages.BoxEventsContents
 {
-    public class DiadocPowerOfAttorneyValidationStatusErrors
+    public class DiadocPowerOfAttorneyValidationStatusError
     {
         public string Code { get; set; }
         public string Text { get; set; }
