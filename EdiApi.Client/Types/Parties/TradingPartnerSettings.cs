1935200cc49a14d23ae9f99fbe3dfb6d46da6aa3
belykhvd <belykhvd@skbkontur.ru>
2022-10-10 19:05:35 +0500

    made PartnerPartyRole nullable

--- EdiApi.Client/Types/Parties/TradingPartnerSettings.cs
+++ EdiApi.Client/Types/Parties/TradingPartnerSettings.cs
@@ -5,7 +5,7 @@ namespace SkbKontur.EdiApi.Client.Types.Parties
     public class TradingPartnerSettings
     {
         public string PartnerPartyId { get; set; }
-        public ConcretePartyRole PartnerPartyRole { get; set; }
+        public ConcretePartyRole? PartnerPartyRole { get; set; }
         public string OwnProviderPartyId { get; set; }
         public string PartnerProviderPartyId { get; set; }
         public TrafficType TrafficType { get; set; }
