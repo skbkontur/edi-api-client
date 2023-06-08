bd452b3b1467645d7856859df72b17a2c9f6ae59
i.borovik <borovik.i@skbkontur.ru>
2023-06-08 20:52:38 +0500

    InboxDiadocDocumentReceiptedBySender

--- EdiApi.Client/Types/Messages/BoxEvents/MessageBoxEventTypeRegistry.cs
+++ EdiApi.Client/Types/Messages/BoxEvents/MessageBoxEventTypeRegistry.cs
@@ -36,6 +36,7 @@ namespace SkbKontur.EdiApi.Client.Types.Messages.BoxEvents
             Register<DiadocDocumentDeliveredEventContent>(MessageBoxEventType.DiadocDocumentDelivered);
             Register<MessageDocumentPackageSignedByRecipientPartiallyOkEventContent>(MessageBoxEventType.DocumentPackageSignedByRecipientPartiallyOk);
             Register<MessageDocumentPackageSignedByMePartiallyOkEventContent>(MessageBoxEventType.DocumentPackageSignedByMePartiallyOk);
+            Register<InboxDiadocDocumentReceiptedBySenderEventContent>(MessageBoxEventType.InboxDiadocDocumentReceiptedBySender);
             Register<OutboxDiadocDocumentReceiptedBySenderEventContent>(MessageBoxEventType.OutboxDiadocDocumentReceiptedBySender);
             Register<OutboxDiadocDocumentRecipientPowerOfAttorneyStatusChangedEventContent>(MessageBoxEventType.OutboxDiadocDocumentRecipientPowerOfAttorneyStatusChanged);
             Register<OutboxDiadocDocumentSenderPowerOfAttorneyStatusChangedEventContent>(MessageBoxEventType.OutboxDiadocDocumentSenderPowerOfAttorneyStatusChanged);
