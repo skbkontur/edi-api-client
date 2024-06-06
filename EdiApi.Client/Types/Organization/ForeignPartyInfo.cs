af4a66b66220ede77d736e6298912fa8661f3606
Sergei Lugovykh <s.lugovykh@skbkontur.ru>
2024-06-06 11:48:36 +0500

    add ForeignPartyInfo to organizations from organization catalogue

---
+++ EdiApi.Client/Types/Organization/ForeignPartyInfo.cs
@@ -0,0 +1,14 @@
+namespace SkbKontur.EdiApi.Client.Types.Organization
+{
+    public class ForeignPartyInfo
+    {
+        /// <summary>Код страны (цифровой по формату ISO 3166-1)</summary>
+        public string CountryCode { get; set; }
+
+        /// <summary>Налоговый номер</summary>
+        public string Tin { get; set; }
+
+        /// <summary>Название иностранной организации</summary>
+        public string Name { get; set; }
+    }
+}
