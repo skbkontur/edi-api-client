433025d713f622f184bbdaf239a506222e397491
belykhvd <belykhvd@gmail.com>
2024-11-11 01:01:27 +0500

    Переход на новую структуру ошибок при проверке МЧД

---
+++ EdiApi.Client/Types/Messages/BoxEventsContents/DiadocPowerOfAttorneyValidationCheckStatus.cs
@@ -0,0 +1,18 @@
+﻿namespace SkbKontur.EdiApi.Client.Types.Messages.BoxEventsContents
+{
+    /// <summary>Результат выполнения проверки МЧД</summary>
+    public enum DiadocPowerOfAttorneyValidationCheckStatus
+    {
+        /// <summary>Значение по умолчанию</summary>
+        UnknownCheckStatus = 0,
+
+        /// <summary>Проверка успешно пройдена</summary>
+        Ok = 1,
+
+        /// <summary>Есть предупреждение</summary>
+        Warning = 2,
+
+        /// <summary>Есть ошибка</summary>
+        Error = 3,
+    }
+}
