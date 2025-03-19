a7dd8a38f4820deafed2c9a9294d9ed08a6aec8c
Sergei Lugovykh <s.lugovykh@skbkontur.ru>
2025-03-19 13:11:57 +0500

    Добавил статусы приложенности МЧД в события подписи документа

---
+++ EdiApi.Client/Types/Messages/BoxEventsContents/DiadocPowerOfAttorneyAttachmentStatusName.cs
@@ -0,0 +1,18 @@
+﻿namespace SkbKontur.EdiApi.Client.Types.Messages.BoxEventsContents
+{
+    /// <summary>Значение статуса приложенности МЧД к подписи</summary>
+    public enum DiadocPowerOfAttorneyAttachmentStatusName
+    {
+        /// <summary>Неизвестный статус</summary>
+        Unknown = 0,
+
+        /// <summary>МЧД приложена</summary>
+        PowerOfAttorneyAttached = 1,
+
+        /// <summary>МЧД не требуется</summary>
+        PowerOfAttorneyNotRequired = 2,
+
+        /// <summary>МЧД требуется</summary>
+        PowerOfAttorneyRequired = 3,
+    }
+}
