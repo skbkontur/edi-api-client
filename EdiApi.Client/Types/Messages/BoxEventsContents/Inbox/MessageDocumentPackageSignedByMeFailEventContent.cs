eaf58b90cfe1df997b81f1c0e855749b92508408
Sergei Lugovykh <s.lugovykh@skbkontur.ru>
2025-03-19 13:11:57 +0500

    Добавил статусы приложенности МЧД в события подписи документа

--- EdiApi.Client/Types/Messages/BoxEventsContents/Inbox/MessageDocumentPackageSignedByMeFailEventContent.cs
+++ EdiApi.Client/Types/Messages/BoxEventsContents/Inbox/MessageDocumentPackageSignedByMeFailEventContent.cs
@@ -3,5 +3,7 @@
     /// <summary>Информация о событии DocumentPackageSignedByMeFail</summary>
     public class MessageDocumentPackageSignedByMeFailEventContent : InboxDiadocEventContentBase
     {
+        /// <summary>Информация о МЧД, приложенной к подписи получателя</summary>
+        public DiadocSignaturePowerOfAttorneyInfo RecipientSignaturePowerOfAttorneyInfo { get; set; }
     }
 }
--- EdiApi.Client/Types/Messages/BoxEventsContents/Inbox/MessageDocumentPackageSignedByMeOkEventContent.cs
+++ EdiApi.Client/Types/Messages/BoxEventsContents/Inbox/MessageDocumentPackageSignedByMeOkEventContent.cs
@@ -3,5 +3,7 @@
     /// <summary>Информация о событии DocumentPackageSignedByMeOk</summary>
     public class MessageDocumentPackageSignedByMeOkEventContent : InboxDiadocEventContentBase
     {
+        /// <summary>Информация о МЧД, приложенной к подписи получателя</summary>
+        public DiadocSignaturePowerOfAttorneyInfo RecipientSignaturePowerOfAttorneyInfo { get; set; }
     }
 }
