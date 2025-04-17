1535388ea0c6fbf261b810f4edd260cefc75db71
Sergei Lugovykh <s.lugovykh@skbkontur.ru>
2025-04-17 18:31:40 +0500

    rm from events that will use УС logic

--- EdiApi.Client/Types/Messages/BoxEventsContents/Inbox/MessageDocumentPackageSignedByMeFailEventContent.cs
+++ EdiApi.Client/Types/Messages/BoxEventsContents/Inbox/MessageDocumentPackageSignedByMeFailEventContent.cs
@@ -3,7 +3,5 @@
     /// <summary>Информация о событии DocumentPackageSignedByMeFail</summary>
     public class MessageDocumentPackageSignedByMeFailEventContent : InboxDiadocEventContentBase
     {
-        /// <summary>Информация о МЧД, приложенной к подписи получателя</summary>
-        public DiadocSignaturePowerOfAttorneyInfo RecipientSignaturePowerOfAttorneyInfo { get; set; }
     }
 }
--- EdiApi.Client/Types/Messages/BoxEventsContents/Outbox/MessageDocumentPackageSignedByRecipientFailEventContent.cs
+++ EdiApi.Client/Types/Messages/BoxEventsContents/Outbox/MessageDocumentPackageSignedByRecipientFailEventContent.cs
@@ -8,8 +8,5 @@
 
         /// <summary>Необходимо извещение о получение на документ со стороны отправителя</summary>
         public bool NeedReceiptBySender { get; set; }
-
-        /// <summary>Информация о МЧД, приложенной к подписи получателя</summary>
-        public DiadocSignaturePowerOfAttorneyInfo RecipientSignaturePowerOfAttorneyInfo { get; set; }
     }
 }
