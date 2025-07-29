ff44a5fc1b1e209ad008a114e4f6fac316aa7d9f
Sergei Lugovykh <s.lugovykh@skbkontur.ru>
2025-07-29 17:33:29 +0500

    Добавил поле "Требуется подписать документ отмены фиксации" для событий AmendmentRequestedEventContent, MessageDiadocRevocationRequestedEventContent

--- EdiApi.Client/Types/Messages/BoxEventsContents/Outbox/AmendmentRequestedEventContent.cs
+++ EdiApi.Client/Types/Messages/BoxEventsContents/Outbox/AmendmentRequestedEventContent.cs
@@ -7,5 +7,11 @@ namespace SkbKontur.EdiApi.Client.Types.Messages.BoxEventsContents.Outbox
     {
         /// <summary>Текст уведомления об уточнении</summary>
         public string? AmendmentRequestMessage { get; set; }
+
+        /// <summary>
+        ///     Требуется подписать документ отмены фиксации в ГИС МТ "Честный ЗНАК".
+        ///     Значение поля актуально на момент возникновения события и не меняется после подписания документа отмены фиксации.
+        /// </summary>
+        public bool? IsGisMtFixationCancellationSigningRequired { get; set; }
     }
 }
--- EdiApi.Client/Types/Messages/BoxEventsContents/Outbox/MessageDiadocRevocationRequestedEventContent.cs
+++ EdiApi.Client/Types/Messages/BoxEventsContents/Outbox/MessageDiadocRevocationRequestedEventContent.cs
@@ -5,5 +5,11 @@
     {
         /// <summary>Информация об аннулировании документа</summary>
         public RevocationInfo RevocationInfo { get; set; }
+
+        /// <summary>
+        ///     Требуется подписать документ отмены фиксации в ГИС МТ "Честный ЗНАК".
+        ///     Значение поля актуально на момент возникновения события и не меняется после подписания документа отмены фиксации.
+        /// </summary>
+        public bool? IsGisMtFixationCancellationSigningRequired { get; set; }
     }
 }
