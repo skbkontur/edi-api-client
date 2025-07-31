cc2a1cb295571831e3e6e65a22b24fb237bf9f23
Sergei Lugovykh <s.lugovykh@skbkontur.ru>
2025-07-31 15:01:34 +0500

    change documentation wording

--- EdiApi.Client/Types/Messages/BoxEventsContents/Outbox/AmendmentRequestedEventContent.cs
+++ EdiApi.Client/Types/Messages/BoxEventsContents/Outbox/AmendmentRequestedEventContent.cs
@@ -9,8 +9,8 @@ namespace SkbKontur.EdiApi.Client.Types.Messages.BoxEventsContents.Outbox
         public string? AmendmentRequestMessage { get; set; }
 
         /// <summary>
-        ///     Требуется подписать документ отмены фиксации в ГИС МТ "Честный ЗНАК".
-        ///     Значение поля актуально на момент возникновения события и не меняется после подписания документа отмены фиксации.
+        ///     Требуется подписать документ отмены фиксации кодов в ГИС МТ "Честный ЗНАК".
+        ///     Значение поля актуально на момент возникновения события и не меняется после подписания документа отмены фиксации кодов.
         /// </summary>
         public bool? IsGisMtFixationCancellationSigningRequired { get; set; }
     }
