436c948771ec1d3083661d8b600cb1c9d04320c1
Ilnur Mukhametov <voda2600@mail.ru>
2025-09-29 11:20:34 +0300

    UpdateVersionDeleteDto
    GetUsersInfoDeleted

--- EdiApi.Client/Types/Parties/UserInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SkbKontur.EdiApi.Client.Types.Parties
-{
-    /// <summary>Информация о пользователе</summary>
-    public class UserInfo
-    {
-        /// <summary>Email</summary>
-        public string Email { get; set; }
-    }
-}
