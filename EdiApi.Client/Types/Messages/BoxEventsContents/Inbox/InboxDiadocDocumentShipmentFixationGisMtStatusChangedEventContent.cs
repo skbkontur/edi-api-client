ccb70a3f6c1edd4a27f1e7d1dc928197746a39a3
Ilnur Mukhametov <voda2600@mail.ru>
2025-10-06 10:28:20 +0300

    Update InboxDiadocDocumentShipmentFixationGisMtStatusChangedEventContent.cs

--- EdiApi.Client/Types/Messages/BoxEventsContents/Inbox/InboxDiadocDocumentShipmentFixationGisMtStatusChangedEventContent.cs
+++ EdiApi.Client/Types/Messages/BoxEventsContents/Inbox/InboxDiadocDocumentShipmentFixationGisMtStatusChangedEventContent.cs
@@ -1,6 +1,6 @@
 namespace SkbKontur.EdiApi.Client.Types.Messages.BoxEventsContents.Inbox
 {
-    /// <summary>Информация о событии InboxDiadocDocumentShipmentFixationGisMtStatusChangedEventContent в ящике отправителя</summary>
+    /// <summary>Информация о событии InboxDiadocDocumentShipmentFixationGisMtStatusChangedEventContent в ящике получателя</summary>
     public class InboxDiadocDocumentShipmentFixationGisMtStatusChangedEventContent : InboxDiadocEventContentBase
     {
         /// <summary>Информация о статусе проверки в ГИС МТ "Честный ЗНАК"</summary>
