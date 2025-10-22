5f460a09d02269f46c9da12a1a04cf9a8af9aef6
Ilnur Mukhametov <voda2600@mail.ru>
2025-10-22 10:11:51 +0300

    event-content-added

---
+++ EdiApi.Client/Types/Messages/BoxEventsContents/Outbox/ChildOutboxEntityMessageEventContent.cs
@@ -0,0 +1,11 @@
+﻿using SkbKontur.EdiApi.Client.Types.Boxes;
+
+namespace SkbKontur.EdiApi.Client.Types.Messages.BoxEventsContents.Outbox
+{
+    /// <summary>Информация о событии ChildOutboxEntityMessage</summary>
+    public class ChildOutboxEntityMessageEventContent : OutboxEventContentBase
+    {
+        /// <summary>Тип транспорта</summary>
+        public TransportType TransportType { get; set; }
+    }
+}
