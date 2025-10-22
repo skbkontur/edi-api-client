bc5efa5b584f9bc2d96034bd6b45a4620573b223
Ilnur Mukhametov <voda2600@mail.ru>
2025-10-22 04:04:09 +0300

    codestyle-fix

--- EdiApi.Client/Types/Messages/BasicMessageMeta.cs
+++ EdiApi.Client/Types/Messages/BasicMessageMeta.cs
@@ -11,10 +11,10 @@
 
         /// <summary>Внутренний идентификатор сообщения: при помощи этого идентификатора можно просмотреть информацию о сообщении в мониторинге сервиса</summary>
         public string DocumentCirculationId { get; set; }
-        
+
         /// <summary>Внутренний идентификатор родительского документа (архива/мультисообщения)</summary>
         public string ParentDocumentCirculationId { get; set; }
-        
+
         /// <summary>Имя исходного файла, из которого было создано сообщение</summary>
         public string SourceFileName { get; set; }
     }
