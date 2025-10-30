7a4f7ceeeb7022054ead3ffa2e757daa5aba5dc6
Sergei Lugovykh <s.lugovykh@skbkontur.ru>
2025-10-30 13:51:47 +0500

    add async methods

--- EdiApi.Client/IBaseEdiApiClient.cs
+++ EdiApi.Client/IBaseEdiApiClient.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading.Tasks;
 
 using SkbKontur.EdiApi.Client.Types.Boxes;
 using SkbKontur.EdiApi.Client.Types.Logistics;
@@ -20,19 +21,27 @@ namespace SkbKontur.EdiApi.Client
         string Authenticate(string login, string password);
 
         PartiesInfo GetAccessiblePartiesInfo(string authToken);
+        Task<PartiesInfo> GetAccessiblePartiesInfoAsync(string authToken);
 
         PartyInfo GetPartyInfo(string authToken, string partyId);
+        Task<PartyInfo> GetPartyInfoAsync(string authToken, string partyId);
 
         PartyInfo GetPartyInfoByGln(string authToken, string partyGln);
+        Task<PartyInfo> GetPartyInfoByGlnAsync(string authToken, string partyGln);
 
         BoxesInfo GetBoxesInfo(string authToken);
+        Task<BoxesInfo> GetBoxesInfoAsync(string authToken);
 
         BoxInfo GetMainApiBox(string authToken, string partyId);
+        Task<BoxInfo> GetMainApiBoxAsync(string authToken, string partyId);
 
         OrganizationCatalogueInfo GetOrganizationCatalogueInfo(string authToken, string partyId);
+        Task<OrganizationCatalogueInfo> GetOrganizationCatalogueInfoAsync(string authToken, string partyId);
 
         PartyInfo GetPartyInfoByDepartmentGln(string authToken, string departmentGln);
+        Task<PartyInfo> GetPartyInfoByDepartmentGlnAsync(string authToken, string departmentGln);
 
         TransportationDocumentIdentifier GetTransportationDocumentIdentifier(string authToken, string partyId);
+        Task<TransportationDocumentIdentifier> GetTransportationDocumentIdentifierAsync(string authToken, string partyId);
     }
 }
--- EdiApi.Client/IMessagesEdiApiClient.cs
+++ EdiApi.Client/IMessagesEdiApiClient.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading.Tasks;
 
 using SkbKontur.EdiApi.Client.Types.Common;
 using SkbKontur.EdiApi.Client.Types.Messages;
@@ -11,21 +12,30 @@ namespace SkbKontur.EdiApi.Client
     public interface IMessagesEdiApiClient : IBaseEdiApiClient
     {
         BoxDocumentsSettings GetBoxDocumentsSettings(string authToken, string boxId);
+        Task<BoxDocumentsSettings> GetBoxDocumentsSettingsAsync(string authToken, string boxId);
 
         MessageData GetMessage(string authToken, string boxId, string messageId);
+        Task<MessageData> GetMessageAsync(string authToken, string boxId, string messageId);
 
         InboxMessageMeta GetInboxMessageMeta(string authToken, string boxId, string messageId);
+        Task<InboxMessageMeta> GetInboxMessageMetaAsync(string authToken, string boxId, string messageId);
 
         OutboxMessageMeta GetOutboxMessageMeta(string authToken, string boxId, string messageId);
+        Task<OutboxMessageMeta> GetOutboxMessageMetaAsync(string authToken, string boxId, string messageId);
 
         OutboxMessageMeta SendMessage(string authToken, string boxId, MessageData messageData);
+        Task<OutboxMessageMeta> SendMessageAsync(string authToken, string boxId, MessageData messageData);
 
         MessageBoxEventBatch GetEvents(string authToken, string boxId, string? exclusiveEventId, uint? count = null);
+        Task<MessageBoxEventBatch> GetEventsAsync(string authToken, string boxId, string? exclusiveEventId, uint? count = null);
 
         MessageBoxEventBatch GetEvents(string authToken, string boxId, DateTime fromDateTime, uint? count = null);
+        Task<MessageBoxEventBatch> GetEventsAsync(string authToken, string boxId, DateTime fromDateTime, uint? count = null);
 
         void MessageDeliveryStarted(string authToken, string boxId, string documentCirculationId);
+        Task MessageDeliveryStartedAsync(string authToken, string boxId, string documentCirculationId);
 
         void MessageDeliveryFinished(string authToken, string boxId, string documentCirculationId, MessageDeliveryResult messageDeliveryResult);
+        Task MessageDeliveryFinishedAsync(string authToken, string boxId, string documentCirculationId, MessageDeliveryResult messageDeliveryResult);
     }
 }
