c1592e518db1f8ad8ae8a341a4eba0bbe8a02346
Sergei Lugovykh <s.lugovykh@skbkontur.ru>
2025-10-30 15:49:43 +0500

    add user-agent header

---
+++ EdiApi.Client/Http/UserAgentProvider.cs
@@ -0,0 +1,20 @@
+﻿#nullable enable
+using System.Runtime.InteropServices;
+
+namespace SkbKontur.EdiApi.Client.Http
+{
+    internal static class UserAgentProvider
+    {
+        static UserAgentProvider()
+        {
+            var sdkVersion = typeof(ClusterClientFactory).Assembly.GetName().Version.ToString();
+            var dotnetDescription = RuntimeInformation.FrameworkDescription.Trim();
+            var osDescription = RuntimeInformation.OSDescription.Trim();
+            userAgentString = $"Kontur.EDI API SDK={sdkVersion};OS={osDescription};DOTNET={dotnetDescription}";
+        }
+
+        public static string GetUserAgent() => userAgentString;
+
+        private static readonly string userAgentString;
+    }
+}
