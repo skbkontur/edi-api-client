4100842e0bbe26d7f7d85d8e051c1e62c90bf0ca
rykalov <rykalov@skbkontur.ru>
2026-05-22 15:21:00 +0500

    Add DiadocLogisticsOrderRequestInfo

---
+++ EdiApi.Client/Types/Messages/BoxEventsContents/V2/DiadocLogisticsOrderRequestInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace SkbKontur.EdiApi.Client.Types.Messages.BoxEventsContents.V2
+{
+    /// <summary>Информация о титуле ЭЗЗ</summary>
+    public class DiadocLogisticsOrderRequestInfo
+    {
+        /// <summary>Содержание операции</summary>
+        public string OperationContent { get; set; }
+
+        /// <summary>Список причин отказа в приёме заказа-заявки</summary>
+        public string[] RejectionReasons { get; set; }
+    }
+}
--- EdiApi.Client/Types/Messages/BoxEventsContents/V2/DiadocParticipantTitle.cs
+++ EdiApi.Client/Types/Messages/BoxEventsContents/V2/DiadocParticipantTitle.cs
@@ -23,5 +23,8 @@
 
         /// <summary>Информация о титуле ЭТрН</summary>
         public DiadocLogisticsWaybillInfo LogisticsWaybillInfo { get; set; }
+
+        /// <summary>Информация о титуле ЭЗЗ</summary>
+        public DiadocLogisticsOrderRequestInfo LogisticsOrderRequestInfo { get; set; }
     }
 }
