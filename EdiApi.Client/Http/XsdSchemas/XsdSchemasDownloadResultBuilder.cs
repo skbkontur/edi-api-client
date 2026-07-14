6544e7abd9f226b916ef01b3940c3a110fc1d04f
belykhvd <belykhvd@gmail.com>
2026-07-15 00:12:00 +0500

    Stream -> byte[]

--- EdiApi.Client/Http/XsdSchemas/XsdSchemasDownloadResultBuilder.cs
+++ EdiApi.Client/Http/XsdSchemas/XsdSchemasDownloadResultBuilder.cs
@@ -8,15 +8,13 @@ namespace SkbKontur.EdiApi.Client.Http.XsdSchemas
     {
         public static XsdSchemasDownloadResult Build(Response response)
         {
-            var contentLength = long.Parse(response.Headers["content-length"]!);
             var (filename, version) = ContentDispositionParser.Parse(response.Headers["content-disposition"]);
 
             return new XsdSchemasDownloadResult
                 {
                     Version = version,
                     FileName = filename,
-                    Length = contentLength,
-                    ContentStream = response.Stream,
+                    Content = response.Content.ToArray(),
                 };
         }
     }
--- EdiApi.Client/Types/XsdSchemas/XsdSchemasDownloadResult.cs
+++ EdiApi.Client/Types/XsdSchemas/XsdSchemasDownloadResult.cs
@@ -1,12 +1,9 @@
-﻿using System.IO;
-
-namespace SkbKontur.EdiApi.Client.Types.XsdSchemas
+﻿namespace SkbKontur.EdiApi.Client.Types.XsdSchemas
 {
     public class XsdSchemasDownloadResult
     {
         public string Version { get; set; }
         public string FileName { get; set; }
-        public long Length { get; set; }
-        public Stream ContentStream { get; set; }
+        public byte[] Content { get; set; }
     }
 }
