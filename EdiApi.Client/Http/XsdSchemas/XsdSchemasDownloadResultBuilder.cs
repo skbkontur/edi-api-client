c86734a1f5f7d7c6c2d8098c9473f7c376f767ec
belykhvd <belykhvd@gmail.com>
2026-07-31 01:20:40 +0500

    more robust version parser

--- EdiApi.Client/Http/XsdSchemas/XsdSchemasDownloadResultBuilder.cs
+++ EdiApi.Client/Http/XsdSchemas/XsdSchemasDownloadResultBuilder.cs
@@ -1,14 +1,29 @@
-﻿using SkbKontur.EdiApi.Client.Types.XsdSchemas;
+﻿using System;
+using System.Globalization;
+using System.Net.Http.Headers;
+using System.Text.RegularExpressions;
+
+using SkbKontur.EdiApi.Client.Types.XsdSchemas;
 
 using Vostok.Clusterclient.Core.Model;
 
 namespace SkbKontur.EdiApi.Client.Http.XsdSchemas
 {
-    public static class XsdSchemasDownloadResultBuilder
+    internal static class XsdSchemasDownloadResultBuilder
     {
-        public static XsdSchemasDownloadResult Build(Response response)
+        internal static XsdSchemasDownloadResult Build(Response response)
         {
-            var (filename, version) = ContentDispositionParser.Parse(response.Headers["content-disposition"]);
+            var contentDisposition = response.Headers["content-disposition"];
+            if (contentDisposition is null)
+                throw new HttpClientServerException("Unexpected server response: content-disposition header missing");
+
+            if (!ContentDispositionHeaderValue.TryParse(contentDisposition, out var contentDispositionHeader))
+                throw new HttpClientServerException("Unexpected server response: content-disposition header has invalid format");
+
+            var filename = contentDispositionHeader.FileName;
+
+            if (!TryExtractVersion(filename, out var version))
+                throw new HttpClientServerException("Unexpected server response: filename format is invalid");
 
             return new XsdSchemasDownloadResult
                 {
@@ -17,5 +32,27 @@ namespace SkbKontur.EdiApi.Client.Http.XsdSchemas
                     Content = response.Content.ToArray(),
                 };
         }
+
+        private static bool TryExtractVersion(string filename, out string version)
+        {
+            version = null;
+
+            if (string.IsNullOrEmpty(filename))
+                return false;
+
+            var match = filenameRegex.Match(filename);
+            if (!match.Success ||
+                !DateTime.TryParseExact(match.Groups["date"].Value, "yyyy.MM.dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out _) ||
+                !int.TryParse(match.Groups["revision"].Value, NumberStyles.None, CultureInfo.InvariantCulture, out _))
+            {
+                return false;
+            }
+
+            version = match.Groups["version"].Value;
+            return true;
+        }
+
+        private static readonly Regex filenameRegex = new Regex(@"^(?<version>(?<date>\d{4}\.\d{2}\.\d{2})\.(?<revision>[1-9]\d{0,9}))(?:-tns)?\.zip$",
+                                                                RegexOptions.Compiled | RegexOptions.CultureInvariant);
     }
 }
